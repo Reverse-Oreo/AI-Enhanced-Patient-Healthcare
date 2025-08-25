89a28f14d4d38165db4902e5595f09c349b3385f
Jwongjs <justinwongzhulian@gmail.com>
2025-08-25 18:11:52 +0800

    Final touch-up

--- backend/nodes/__init__.py
+++ backend/nodes/__init__.py
@@ -2,7 +2,6 @@ from .llm_diagnosis_node import LLMDiagnosisNode
 from .follow_up_interaction_node import FollowUpInteractionNode
 from .image_classification_node import ImageClassificationNode
 from .overall_analysis_node import OverallAnalysisNode
-from .healthcare_treatment_recommendation_node import HealthcareRecommendationNode
 from .medical_report_node import MedicalReportNode
 
 
@@ -11,6 +10,5 @@ __all__ = [
     "ImageClassificationNode",
     "FollowUpInteractionNode",
     "OverallAnalysisNode",
-    "HealthcareRecommendationNode",
     "MedicalReportNode"
 ]
